c100ec6ebcba7924ad0a7707d5d8143854bb2168
vineet <vineettanwar1998@gmail.com>
2021-10-31 23:26:05 +0530

    implemented basic test case

--- src/DBConnection/db.js
+++ src/DBConnection/db.js
@@ -1,14 +1,14 @@
 import { client } from '../index';
 
-const DBQuery = (query, data) => {return new Promise((resolve, reject) => {
-    return client.query(query, data, (error, results) => {
-            if (error) {
-                resolve(null)
-            }
-            resolve(results)
-        })
-
-  });
+const DBQuery = (query, data) => {
+    return new Promise((resolve, reject) => {
+        return client.query(query, data, (error, results) => {
+                if (error) {
+                    resolve(null)
+                }
+                resolve(results)
+            })
+    });
 }
 
 export default DBQuery;
