3c0e7aea15f1646c731ed1d7233a841185138b72
vineet <vineettanwar1998@gmail.com>
2021-11-01 12:22:13 +0530

    optimised the code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import Koa from 'koa'
 import Router from 'koa-router'
-import priceAPIs from './pricingAPIs';
-import machineAPIs from './machineAPIs';
+import bodyParser from 'koa-bodyparser';
+import getPricingModelList, { getPricingModelByID, getPricesList } from './pricingAPIs/getPrice';
+import savePriceModelData, { savePriceForPriceModel } from './pricingAPIs/savePrice';
+import updatePricesByID from './pricingAPIs/updatePriceModel';
+import deletePriceData from './pricingAPIs/deletePriceData';
+import saveMachineData from './machineAPIs/saveMachineData';
+import updateMachineByID from './machineAPIs/updateMachineByID';
+import deleteMachineData from './machineAPIs/deleteMachineData';
+import getMachinePriceList from './machineAPIs/getMachinePriceList';
 
 const app = new Koa()
 const PORT = process.env.PORT || 1337
@@ -21,11 +28,58 @@ client.connect(function(err) {
   console.log("Connected!");
 });
 
-priceAPIs(router);
-machineAPIs(router);
+router
+    .use(bodyParser())
+    .get('/pricing-models', async(ctx, next) => {
+        ctx.body =  await getPricingModelList(ctx, next)
+    })
+
+    .get('/pricing-models/:id/prices', async(ctx, next) => {
+        ctx.body =  await getPricesList(ctx, next)
+    })
+
+    .get('/pricing-models/:id', async(ctx, next) => {
+        ctx.body =  await getPricingModelByID(ctx, next)
+    })
+
+    .put('/pricing-models/:id', async(ctx, next) => {
+        ctx.body =  await updatePricesByID(ctx, next)
+    })
+
+    .post('/pricing-models', async(ctx, next) => {
+        ctx.body =  await savePriceModelData(ctx, next)
+    })
+
+    .post('/pricing-models/:id/prices', async(ctx, next) => {
+        ctx.body =  await savePriceForPriceModel(ctx, next)
+    })
+
+    .delete('/pricing-models/:pmID/prices/:priceID', async(ctx, next) => {
+        ctx.body =  await deletePriceData(ctx, next)
+    })
+
+    // machine apis starts from here
+    .get('/machines/:machineID/prices', async(ctx, next) => {
+        ctx.body =  await getMachinePriceList(ctx, next)
+    })
+
+    .put('/machines/:machineID/prices/:pmID', async(ctx, next) => {
+        ctx.body =  await updateMachineByID(ctx, next)
+    })
+
+    .post('/machines', async(ctx, next) => {
+        ctx.body =  await saveMachineData(ctx, next)
+    })
+    
+    .delete('/machines/:machineID/prices/:pmID', async(ctx, next) => {
+        ctx.body =  await deleteMachineData(ctx, next)
+    })
 
 app
   .use(router.routes())
   .listen(PORT, () =>
     console.log(`Server listening on port ${PORT}`)
-  );
+  )
+
+
+module.exports = app;
